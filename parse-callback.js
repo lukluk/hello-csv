9faf21ce24a6b2111d94cab072f574bc767c47ea
Lukluk <luklukaha@gmail.com>
2016-02-21 08:11:23 +0700

    lukluk solutions
    > what about naive function?
    
    ```
    for (let index in parsed)
    ```
    not necessary need increment for index local var
    ```
    index++
    ```
    because it was increment by default

--- parse-callback.js
+++ parse-callback.js
@@ -1,10 +1,9 @@
-'use strict';
+'use strict'
 
-const debug = require('debug')('hello');
-
-const fs = require('fs');
-const parse = require('csv-parse');
-const helper = require('./helper');
+const debug = require('debug')('hello')
+const fs = require('fs')
+const parse = require('csv-parse')
+const helper = require('./helper')
 
 // 0. Naïve
 
@@ -12,42 +11,41 @@ function naive() {
     fs.readFile(__dirname + '/sample.csv', function thenParse(err, loadedCsv) {
 
         parse(loadedCsv, function transformEachLine(err, parsed) {
-
+            //  console.log(parsed)
             for (let index in parsed) {
 
-                let line = parsed[index];
+                let line = parsed[index]
 
                 // FIXME: Put your transformation here
+                line = helper.mergeNameArr(line)
 
                 if (index > 0) {
-                    debug(`sending data index: ${index - 1}`);
-
+                    debug(`sending data index: ${index - 1}`)
                     helper.sendSms(line, function afterSending(err, sendingStatus) {
-                        let lineToLog;
+                        let lineToLog
                         if (err) {
-                            debug(err.message);
+                            debug(err.message)
 
                             lineToLog = {
                                 sendingStatus,
                                 line,
-                            };
+                            }
                         }
 
                         if (lineToLog) {
                             helper.logToS3(lineToLog, function afterLogging(err, loggingStatus) {
                                 if (err) {
-                                    debug(err.message);
+                                    debug(err.message)
                                 }
-                            });
+                            })
                         }
-                    });
+                    })
                 }
 
-                index++;
+                //index++
             }
-        });
-    });
+        })
+    })
 }
 
-naive();
-
+naive()
--- parse-promise.js
+++ parse-promise.js
@@ -1,1 +1,71 @@
-// please use promise approach to fight the naive one in parse-callback.js
+'use strict';
+const parse = require('csv-parse');
+const readline = require('readline');
+const helper = require('./helper');
+const fs = require('fs');
+
+var firstLine = true;
+const debug = require('debug');
+var _success = debug('success');
+var _error = debug('error');
+
+function sendSms(data) {
+  return new Promise(function(success, error) {
+    helper.sendSms(data, function(err, sendingStatus) {
+      if (err) error(err);
+      else {
+        _success('success send sms', sendingStatus)
+        success({
+          sendingStatus,
+          data
+        })
+      }
+    })
+  })
+}
+
+function logS3(data) {
+  return new Promise(function(success, error) {
+    helper.logToS3(data, function(err, response) {
+      if (err) error(err);
+      else {
+        _success('success send log to s3', response)
+        success(response)
+      }
+    })
+  })
+
+}
+
+function parseCsv(line) {
+  return new Promise(function(success, error) {
+    parse(line, function(err, output) {
+      if (err) error(err);
+      else success(line, output[0])
+    })
+  })
+}
+
+function onError(err) {
+  _error('error ', err)
+  return new Error(err)
+}
+
+function parsePromise() {
+  var rl = readline.createInterface({
+    input: fs.createReadStream('./sample.csv'),
+  });
+
+  rl.on('line', function(line) {
+    if (firstLine) {
+      firstLine = false;
+      return false;
+    }
+
+    var transformLine = helper.mergeNameStr(line)
+    parseCsv(transformLine).then(sendSms).then(logS3).catch(onError)
+  })
+
+}
+
+parsePromise()
